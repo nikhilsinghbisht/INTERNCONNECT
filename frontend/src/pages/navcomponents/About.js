5b243e52f0d4db52a195663c163531ec216c7cf9
kartik poojari <kartikpoojari29@gmail.com>
2024-03-23 17:15:40 +0530

    vercel json added

--- frontend/src/pages/navcomponents/About.js
+++ frontend/src/pages/navcomponents/About.js
@@ -21,19 +21,38 @@ function about() {
           <div className="description">
             <h1 className="about-title">About</h1>
             <p className="">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Aspernatur atque perferendis, laudantium quod architecto velit ad
-              officiis facere eveniet in fuga fugiat delectus rerum doloribus
-              quos consectetur unde, expedita, quibusdam corporis impedit quia
-              sequi aliquid sit. Ducimus labore molestias odio nam
-              necessitatibus laboriosam vero saepe enim nobis. Repudiandae
-              quidem, sint earum dolorum consequuntur dignissimos excepturi
-              mollitia omnis aliquid, corporis, unde!
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Dicta doloribus veniam impedit, enim!</li>
-              <li>Quod, facilis cupiditate repellat voluptas.</li>
-              <li>Quae impedit id maxime fugiat.</li>
-              <li>Esse aut iste dolor. In.</li>
+              InternConnect is committed to revolutionizing the intern
+              management landscape. We are dedicated to providing a seamless
+              software solution that simplifies every aspect of intern
+              management, from recruitment to task assignment. Our mission is to
+              empower organizations to efficiently nurture talent while offering
+              interns enriching experiences. With a focus on transparency and
+              innovation, InternConnect strives to be the preferred platform for
+              intern management globally. Join us in shaping the future of
+              internships.
+              <p>
+                <br></br>
+              </p>
+              <li>
+                Mission-driven: Committed to streamlining intern management
+                processes.
+              </li>
+              <li>
+                Excellence: Delivering top-notch software solutions for optimal
+                efficiency.
+              </li>
+              <li>
+                Customer-centric: Prioritizing client needs and feedback for
+                continuous improvement.
+              </li>
+              <li>
+                Innovation: Embracing technological advancements to stay ahead
+                in the industry.
+              </li>
+              <li>
+                Collaboration: Working closely with organizations and
+                educational institutions for mutual benefit.
+              </li>
             </p>
           </div>
         </div>
@@ -44,85 +63,93 @@ function about() {
           <div class="row d-flex justify-content-center">
             <div class="col-md-8 pb-40 header-text">
               <h2>Why InternConnect</h2>
-              <p> Lorem ipsum dolor</p>
+              <h3> Join Now</h3>
             </div>
           </div>
           <div class="row">
             <div class="col-lg-4 col-md-6">
               <div class="single-service">
                 <h4>
-                  <span class="lnr lnr-user"></span> Lorem ipsum
+                  <span class="lnr lnr-user"></span> Seamless Intern Management
                 </h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aspernatur atque perferendis, laudantium quod architecto velit
-                  ad officiis facere eveniet in fuga fugiat delectus rerum
-                  doloribus quos consectetur unde, sequi aliquid sit.
+                  InternConnect offers a comprehensive solution for all your
+                  intern management needs. From recruitment to task assignment,
+                  our platform streamlines processes, saving you time and
+                  resources while ensuring a smooth experience for both
+                  organizations and interns.
                 </p>
               </div>
             </div>
             <div class="col-lg-4 col-md-6">
               <div class="single-service">
                 <h4>
-                  <span class="lnr lnr-license"></span> Lorem ipsum
+                  <span class="lnr lnr-license"></span> Enhanced Efficiency
                 </h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aspernatur atque perferendis, laudantium quod architecto velit
-                  ad officiis facere eveniet in fuga fugiat delectus rerum
-                  doloribus quos consectetur unde, sequi aliquid sit.
+                  With InternConnect, say goodbye to manual paperwork and
+                  tedious administrative tasks. Our innovative software
+                  automates processes, increasing efficiency and allowing you to
+                  focus on what matters most - nurturing talent and fostering
+                  growth within your organization.
                 </p>
               </div>
             </div>
             <div class="col-lg-4 col-md-6">
               <div class="single-service">
                 <h4>
-                  <span class="lnr lnr-phone"></span> Lorem ipsum
+                  <span class="lnr lnr-phone"></span> Transparent Communication
                 </h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aspernatur atque perferendis, laudantium quod architecto velit
-                  ad officiis facere eveniet in fuga fugiat delectus rerum
-                  doloribus quos consectetur unde, sequi aliquid sit.
+                  InternConnect promotes transparent communication channels
+                  between organizations and interns. Our platform facilitates
+                  clear expectations, feedback loops, and progress tracking,
+                  fostering a collaborative and supportive environment for all
+                  parties involved.
                 </p>
               </div>
             </div>
             <div class="col-lg-4 col-md-6">
               <div class="single-service">
                 <h4>
-                  <span class="lnr lnr-rocket"></span> Lorem ipsum
+                  <span class="lnr lnr-rocket"></span> Tailored Solutions
                 </h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aspernatur atque perferendis, laudantium quod architecto velit
-                  ad officiis facere eveniet in fuga fugiat delectus rerum
-                  doloribus quos consectetur unde, sequi aliquid sit.
+                  InternConnect understands that every organization has unique
+                  needs. Our platform offers customizable features and flexible
+                  options to adapt to your specific intern management
+                  requirements, ensuring that you have the tools you need to
+                  succeed.
                 </p>
               </div>
             </div>
             <div class="col-lg-4 col-md-6">
               <div class="single-service">
                 <h4>
-                  <span class="lnr lnr-diamond"></span> Lorem ipsum
+                  <span class="lnr lnr-diamond"></span> Cost-Effective
+                  Investment
                 </h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aspernatur atque perferendis, laudantium quod architecto velit
-                  ad officiis facere eveniet in fuga fugiat delectus rerum
-                  doloribus quos consectetur unde, sequi aliquid sit.
+                  Investing in InternConnect is not just about improving intern
+                  management; it's about making a cost-effective decision for
+                  your organization's future. Our platform offers value for
+                  money by streamlining processes and optimizing resource
+                  allocation.
                 </p>
               </div>
             </div>
             <div class="col-lg-4 col-md-6">
               <div class="single-service">
                 <h4>
-                  <span class="lnr lnr-bubble"></span> Lorem ipsum
+                  <span class="lnr lnr-bubble"></span> Future-Proofing Internship 
                 </h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aspernatur atque perferendis, laudantium quod architecto velit
-                  ad officiis facere eveniet in fuga fugiat delectus rerum
-                  doloribus quos consectetur unde, sequi aliquid sit.
+                  By choosing InternConnect, you're not just investing in a
+                  solution for today; you're future-proofing your internship
+                  program. Our platform evolves with industry trends and
+                  technological advancements, ensuring that your intern
+                  management processes remain efficient and effective in the
+                  long run.
                 </p>
               </div>
             </div>
